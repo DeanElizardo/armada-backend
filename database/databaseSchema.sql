917f6384fadb2ca855b3321939767d7849eaa610
Dean Elizardo <del@delizardo.com>
2023-04-07 08:50:56 -0500

    simplified schema

--- database/databaseSchema.sql
+++ database/databaseSchema.sql
@@ -1,11 +1,12 @@
-CREATE DATABASE "Armada";
+CREATE DATABASE armada;
+\c armada;
 
 -- Connect to the database before creating tables!
 -- Also, if you need to drop them, use DROP TABLE "Users"; (Have to wrap in quotes!)
 -- The quotes are used to preserve case sensitivity.
 -- In the prisma schema, make sure to lowercase all the model entries (first column)
 
-CREATE TABLE "public"."User" (
+CREATE TABLE users (
   uuid VARCHAR(255) PRIMARY KEY NOT NULL,
   username VARCHAR(255) UNIQUE NOT NULL,
   email VARCHAR(255) UNIQUE NOT NULL,
@@ -14,43 +15,43 @@ CREATE TABLE "public"."User" (
   isAdmin BOOLEAN NOT NULL DEFAULT FALSE
 );
 
-CREATE TABLE "public"."Cohort" (
+CREATE TABLE cohort (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) UNIQUE NOT NULL
 );
 
-CREATE TABLE "public"."Course" (
+CREATE TABLE course (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) UNIQUE NOT NULL,
   cohortId INTEGER NOT NULL,
-  FOREIGN KEY (cohortId) REFERENCES "public"."Cohort"(id),
+  FOREIGN KEY (cohortId) REFERENCES cohort(id),
   UNIQUE (name, cohortId)
 );
 
-CREATE TABLE "public"."Workspace" (
+CREATE TABLE workspace (
   uuid VARCHAR(255) PRIMARY KEY NOT NULL,
   desiredCount INTEGER NOT NULL,
   website VARCHAR(255) NOT NULL,
   userId VARCHAR(255) NOT NULL,
   courseId INTEGER NOT NULL,
-  FOREIGN KEY (courseId) REFERENCES "public"."Course"(id) ON DELETE CASCADE,
-  FOREIGN KEY (userId) REFERENCES "public"."User"(uuid) ON DELETE CASCADE
+  FOREIGN KEY (courseId) REFERENCES course(id) ON DELETE CASCADE,
+  FOREIGN KEY (userId) REFERENCES users(uuid) ON DELETE CASCADE
 );
 
-CREATE TABLE "public"."User_Cohort" (
+CREATE TABLE user_cohort (
   userId VARCHAR(255) NOT NULL,
   cohortId INTEGER NOT NULL,
-  FOREIGN KEY (userId) REFERENCES "public"."User"(uuid) ON DELETE CASCADE,
-  FOREIGN KEY (cohortId) REFERENCES "public"."Cohort"(id) ON DELETE CASCADE,
+  FOREIGN KEY (userId) REFERENCES users(uuid) ON DELETE CASCADE,
+  FOREIGN KEY (cohortId) REFERENCES cohort(id) ON DELETE CASCADE,
   PRIMARY KEY (userId, cohortId)
 );
 
-CREATE TABLE "public"."User_Course" (
+CREATE TABLE user_course (
   userId VARCHAR(255) NOT NULL,
   courseId INTEGER NOT NULL,
-  FOREIGN KEY (userId) REFERENCES "public"."User"(uuid) ON DELETE CASCADE,
-  FOREIGN KEY (courseId) REFERENCES "public"."Course"(id) ON DELETE CASCADE,
+  FOREIGN KEY (userId) REFERENCES users(uuid) ON DELETE CASCADE,
+  FOREIGN KEY (courseId) REFERENCES course(id) ON DELETE CASCADE,
   PRIMARY KEY (userId, courseId)
 );
 
-INSERT INTO "User" (uuid, username, email, firstname, lastname, isadmin) VALUES ('original_admin', 'armadaadmin', 'thefourofours@gmail.com', 'Armada', 'Admin', TRUE);
+INSERT INTO users (uuid, username, email, firstname, lastname, isadmin) VALUES ('original_admin', 'armadaadmin', 'thefourofours@gmail.com', 'armada', 'admin', TRUE);
