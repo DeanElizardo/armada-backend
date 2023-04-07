f949fa64fcf9ee088760fd539ca267a0ee58ac2a
Dean Elizardo <del@delizardo.com>
2023-04-07 09:11:28 -0500

    Rewrote backend in go; CRUD for cohorts

--- src/routers/cohort.go
+++ src/routers/cohort.go
@@ -4,6 +4,7 @@ import (
 	sys "armadabackend/logging"
 	db "armadabackend/services/databaseServices"
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
@@ -12,30 +13,173 @@ type cohortWBody struct {
 	Result  []db.CohortRecord `json:"result"`
 }
 
+type cohortRBody struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+// Create
+func cohortCreate(w http.ResponseWriter, r *http.Request) {
+	var rBody cohortRBody
+	err := json.NewDecoder(r.Body).Decode(&rBody)
+	if err != nil {
+		sys.Log.Errorf("cohortCreate: failed to read request body: %s", err)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(nil)
+		return
+	}
+
+	cohort, err := db.CreateCohortRecord(rBody.Name)
+	if err != nil {
+		sys.Log.Errorf("cohortCreate: failed to create new cohort: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write(nil)
+		return
+	}
+	sys.Log.Infof("successfully created cohort (id=%d, name=%s)", cohort[0].ID, cohort[0].Name)
+
+	data := cohortWBody{
+		Message: "Success: created new cohort",
+		Result:  cohort,
+	}
+	wBody, err := json.Marshal(data)
+	if err != nil {
+		sys.Log.Errorf("cohortCreate: failed to marshal data: %s", err)
+	}
+
+	w.Write(wBody)
+}
+
+// Read
 func cohortGetAll(w http.ResponseWriter, r *http.Request) {
 	cohorts, err := db.GetAllCohortRecords()
 	if err != nil {
-		sys.Log.Errorf("failed to get all cohorts: %s", err)
+		sys.Log.Errorf("cohortGetAll: failed to get all cohorts: %s", err)
 		w.WriteHeader(http.StatusInternalServerError)
-		w.Header().Set("Content-Type", "application/json")
-		data := cohortWBody{
-			Message: "Internal server error",
-			Result:  []db.CohortRecord{},
-		}
-		body, err := json.Marshal(data)
-		if err != nil {
-			sys.Log.Errorf("failed to marshal data: %s", err)
-		}
-		w.Write(body)
+		w.Write(nil)
+		return
 	}
 	sys.Log.Info("successfully fetched all cohorts")
+
 	data := cohortWBody{
 		Message: "Success: fetched all cohorts",
 		Result:  cohorts,
 	}
-	body, err := json.Marshal(data)
+	wBody, err := json.Marshal(data)
 	if err != nil {
-		sys.Log.Errorf("failed to marshal data: %s", err)
+		sys.Log.Errorf("cohortGetAll: failed to marshal data: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write(nil)
+		return
 	}
-	w.Write(body)
+
+	w.Write(wBody)
+}
+
+func cohortGetSingle(w http.ResponseWriter, r *http.Request) {
+	var rBody cohortRBody
+	err := json.NewDecoder(r.Body).Decode(&rBody)
+	if err != nil {
+		sys.Log.Errorf("cohortGetSingle: failed to read requst body: %s", err)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(nil)
+		return
+	}
+
+	cohort, err := db.GetSingleCohort(rBody.ID)
+	if err != nil {
+		sys.Log.Errorf("cohortGetSingle: failed to get cohort: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write(nil)
+		return
+	}
+	sys.Log.Infof("successfully fetched single cohort (id=%d)", rBody.ID)
+
+	data := cohortWBody{
+		Message: "success: fetched single cohort",
+		Result:  cohort,
+	}
+	wBody, err := json.Marshal(data)
+	if err != nil {
+		sys.Log.Errorf("cohortGetSingle: unable to marshal data: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write(nil)
+		return
+	}
+
+	w.Write(wBody)
+}
+
+// Update
+func cohortUpdate(w http.ResponseWriter, r *http.Request) {
+	var rBody cohortRBody
+	err := json.NewDecoder(r.Body).Decode(&rBody)
+	if err != nil {
+		sys.Log.Errorf("cohortUpdate: failed to read request body: %s", err)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(nil)
+		return
+	}
+
+	cohort, err := db.UpdateCohort(rBody.ID, rBody.Name)
+	if err != nil {
+		sys.Log.Errorf("cohortUpdate: failed to update cohort '%s %s': %s", rBody.ID, rBody.Name, err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write(nil)
+		return
+	}
+	sys.Log.Infof("successfully updated cohort (id=%s)", rBody.ID)
+
+	data := cohortWBody{
+		Message: "success: updated cohort",
+		Result:  cohort,
+	}
+	wBody, err := json.Marshal(data)
+	if err != nil {
+		sys.Log.Errorf("cohortUpdate: failed to marshal data: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write(nil)
+		return
+	}
+
+	w.Write(wBody)
+}
+
+// Delete
+func cohortDelete(w http.ResponseWriter, r *http.Request) {
+	var rBody cohortRBody
+	err := json.NewDecoder(r.Body).Decode(&rBody)
+	if err != nil {
+		sys.Log.Errorf("cohortDelete: failed to read request body: %s", err)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(nil)
+		return
+	}
+
+	deleteCount, err := db.DeleteCohort(rBody.ID)
+	if err != nil {
+		sys.Log.Errorf("cohortDelete: failed to delete cohort '%s %s': %s", rBody.ID, rBody.Name, err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write(nil)
+		return
+	}
+	if deleteCount != 0 {
+		sys.Log.Infof("successfully deleted cohort (id=%s)", rBody.ID)
+	} else {
+		sys.Log.Info("completed deletion action; no records with id=%s removed; possible record D.N.E.", rBody.ID)
+	}
+
+	data := cohortWBody{
+		Message: fmt.Sprintf("success: deleted %d cohorts", deleteCount),
+		Result:  []db.CohortRecord{},
+	}
+	wBody, err := json.Marshal(data)
+	if err != nil {
+		sys.Log.Errorf("cohortDelete: unable to marshal data: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write(nil)
+		return
+	}
+
+	w.Write(wBody)
 }
--- src/services/databaseServices/cohortActions.go
+++ src/services/databaseServices/cohortActions.go
@@ -1,10 +1,45 @@
 package databaseServices
 
 type CohortRecord struct {
-	ID   int64
+	ID   int
 	Name string
 }
 
+// Create
+func CreateCohortRecord(cohortName string) ([]CohortRecord, error) {
+	var cohortRecord []CohortRecord
+	var id int
+	db, err := connectDB()
+	if err != nil {
+		return cohortRecord, err
+	}
+	defer db.Close()
+
+	sqlStatement := `INSERT INTO cohort (name) VALUES ($1) RETURNING id`
+	err = db.QueryRow(sqlStatement, cohortName).Scan(&id)
+	if err != nil {
+		return cohortRecord, err
+	}
+
+	sqlStatement = `SELECT * FROM cohort WHERE id=$1`
+	row, err := db.Query(sqlStatement, id)
+	if err != nil {
+		return cohortRecord, err
+	}
+	for row.Next() {
+		var id int
+		var name string
+		err = row.Scan(&id, &name)
+		if err != nil {
+			return cohortRecord, err
+		}
+		record := CohortRecord{id, name}
+		cohortRecord = append(cohortRecord, record)
+	}
+	return cohortRecord, nil
+}
+
+// Read
 func GetAllCohortRecords() ([]CohortRecord, error) {
 	var cohortRecords []CohortRecord
 
@@ -22,7 +57,7 @@ func GetAllCohortRecords() ([]CohortRecord, error) {
 	defer rows.Close()
 
 	for rows.Next() {
-		var id int64
+		var id int
 		var name string
 		err = rows.Scan(&id, &name)
 		if err != nil {
@@ -33,4 +68,80 @@ func GetAllCohortRecords() ([]CohortRecord, error) {
 	}
 
 	return cohortRecords, nil
+}
+
+func GetSingleCohort(id int) ([]CohortRecord, error) {
+	var cohortRecord []CohortRecord
+	db, err := connectDB()
+	if err != nil {
+		return cohortRecord, err
+	}
+	defer db.Close()
+
+	sqlStatement := `SELECT * FROM cohort WHERE id=$1`
+	row, err := db.Query(sqlStatement, id)
+	if err != nil {
+		return cohortRecord, err
+	}
+	defer row.Close()
+
+	for row.Next() {
+		var id int
+		var name string
+		err = row.Scan(&id, &name)
+		if err != nil {
+			return cohortRecord, err
+		}
+		record := CohortRecord{id, name}
+		cohortRecord = append(cohortRecord, record)
+	}
+
+	return cohortRecord, nil
+}
+
+// Update
+func UpdateCohort(id int, name string) ([]CohortRecord, error) {
+	var cohortRecord []CohortRecord
+	db, err := connectDB()
+	if err != nil {
+		return cohortRecord, err
+	}
+	defer db.Close()
+
+	sqlStatement := `UPDATE cohort SET name=$2 WHERE id=$1 RETURNING id, name`
+	var updatedID int
+	var updatedName string
+	err = db.QueryRow(sqlStatement, id, name).Scan(&updatedID, &updatedName)
+	if err != nil {
+		return cohortRecord, err
+	}
+
+	record := CohortRecord{
+		ID:   updatedID,
+		Name: updatedName,
+	}
+	cohortRecord = append(cohortRecord, record)
+
+	return cohortRecord, nil
+}
+
+// Delete
+func DeleteCohort(id int) (int64, error) {
+	var deleteCount int64
+	db, err := connectDB()
+	if err != nil {
+		return deleteCount, err
+	}
+	defer db.Close()
+
+	sqlStatement := `DELETE FROM cohort WHERE id=$1`
+	response, err := db.Exec(sqlStatement, id)
+	if err != nil {
+		return deleteCount, err
+	}
+	deleteCount, err = response.RowsAffected()
+	if err != nil {
+		return deleteCount, err
+	}
+	return deleteCount, nil
 }
