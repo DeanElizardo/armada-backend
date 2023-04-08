85348af674fddcd764e835fe0cb83564e91c3c4c
Dean Elizardo <del@delizardo.com>
2023-04-08 08:39:14 -0500

    gave tables more conventional name

--- database/databaseSchema.sql
+++ database/databaseSchema.sql
@@ -15,42 +15,42 @@ CREATE TABLE users (
   isAdmin BOOLEAN NOT NULL DEFAULT FALSE
 );
 
-CREATE TABLE cohort (
+CREATE TABLE cohorts (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) UNIQUE NOT NULL
 );
 
-CREATE TABLE course (
+CREATE TABLE courses (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) UNIQUE NOT NULL,
   cohortId INTEGER NOT NULL,
-  FOREIGN KEY (cohortId) REFERENCES cohort(id),
+  FOREIGN KEY (cohortId) REFERENCES cohorts(id),
   UNIQUE (name, cohortId)
 );
 
-CREATE TABLE workspace (
+CREATE TABLE workspaces (
   uuid VARCHAR(255) PRIMARY KEY NOT NULL,
   desiredCount INTEGER NOT NULL,
   website VARCHAR(255) NOT NULL,
   userId VARCHAR(255) NOT NULL,
   courseId INTEGER NOT NULL,
-  FOREIGN KEY (courseId) REFERENCES course(id) ON DELETE CASCADE,
+  FOREIGN KEY (courseId) REFERENCES courses(id) ON DELETE CASCADE,
   FOREIGN KEY (userId) REFERENCES users(uuid) ON DELETE CASCADE
 );
 
-CREATE TABLE user_cohort (
+CREATE TABLE users_cohorts (
   userId VARCHAR(255) NOT NULL,
   cohortId INTEGER NOT NULL,
   FOREIGN KEY (userId) REFERENCES users(uuid) ON DELETE CASCADE,
-  FOREIGN KEY (cohortId) REFERENCES cohort(id) ON DELETE CASCADE,
+  FOREIGN KEY (cohortId) REFERENCES cohorts(id) ON DELETE CASCADE,
   PRIMARY KEY (userId, cohortId)
 );
 
-CREATE TABLE user_course (
+CREATE TABLE users_courses (
   userId VARCHAR(255) NOT NULL,
   courseId INTEGER NOT NULL,
   FOREIGN KEY (userId) REFERENCES users(uuid) ON DELETE CASCADE,
-  FOREIGN KEY (courseId) REFERENCES course(id) ON DELETE CASCADE,
+  FOREIGN KEY (courseId) REFERENCES courses(id) ON DELETE CASCADE,
   PRIMARY KEY (userId, courseId)
 );
 
